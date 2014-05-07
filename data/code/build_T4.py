de165f58ba0048b1a7b31637a67381f33962c668
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2014-05-07 10:23:51 -0400

    Added fixed T4 structure.

--- data/code/build_T4.py
+++ data/code/build_T4.py
@@ -12,34 +12,14 @@ temperature = 293.
 pressure = 1.0 * u.atmospheres
 
 
-fixer = pdbfixer.PDBFixer("./1am7_chain0_renamed.pdb")
+fixer = pdbfixer.PDBFixer("./1am7.pdb")
+
+fixer.findMissingResidues()
 fixer.findNonstandardResidues()
 fixer.replaceNonstandardResidues()
-fixer.findMissingResidues()
 fixer.findMissingAtoms()
-fixer.addMissingHydrogens()
 fixer.addMissingAtoms()
-fixer.addSolvent(padding=padding)
-
-topology = fixer.topology
-
-system = ff.createSystem(topology, nonbondedMethod=app.PME, nonbondedCutoff=cutoff, constraints=app.HBonds)
-
-integrator = mm.LangevinIntegrator(temperature, 1.0 / u.picoseconds, 1.0 * u.femtoseconds)
-system.addForce(mm.MonteCarloBarostat(pressure, temperature, 25))
-simulation = app.Simulation(topology, system, integrator)
-simulation.context.setPositions(fixer.positions)
-print('Minimizing...')
-simulation.minimizeEnergy()
-
-simulation.context.setVelocitiesToTemperature(temperature)
-print('Equilibrating...')
-simulation.step(50000)
-state = simulation.context.getState(getPositions=True, getParameters=True)
-
-positions = state.getPositions()
-vectors = state.getPeriodicBoxVectors()
-vectors = tuple([v[i] / u.nanometers for (i,v) in enumerate(vectors)])
-vectors = u.Quantity(vectors, u.nanometer)
-topology.setUnitCellDimensions(vectors)
-app.PDBFile.writeFile(topology, positions, open("./boxes/%s" % base_filename, 'w'))
+fixer.removeHeterogens(True)
+fixer.addMissingHydrogens()
+fixer.removeChains([1, 2, 3, 4, 5])
+app.PDBFile.writeFile(fixer.topology, fixer.positions, open("1am7_fixed.pdb", 'w'))
