3961af23ee1d80c1538f42d28fa016e5d688d722
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2014-05-10 14:24:11 -0400

    Added ubiquitin stuff.

---
+++ data/code/simulate_ubiquitin.py
@@ -0,0 +1,42 @@
+from simtk.openmm import app
+import simtk.openmm as mm
+from simtk import unit as u
+import mdtraj.reporters
+
+cutoff = 0.95 * u.nanometers
+output_frequency = 25000
+n_steps = 500000000
+temperature = 298. 
+pressure = 1.0 * u.atmospheres
+
+platform_name = "CUDA"
+pdb_filename = "./1d3z_equil.pdb"
+dcd_filename = "./1d3z.dcd"
+log_filename = "./1d3z.log"
+
+traj = mdtraj.load(pdb_filename)
+top, bonds = traj.top.to_dataframe()
+atom_indices = top.index[top.chainID == 0].values
+
+pdb = app.PDBFile(pdb_filename)
+topology = pdb.topology
+positions = pdb.positions
+
+ff = app.ForceField('amber99sbnmr.xml', 'tip3p-fb.xml')
+
+platform = mm.Platform.getPlatformByName(platform_name)
+
+system = ff.createSystem(topology, nonbondedMethod=app.PME, nonbondedCutoff=cutoff, constraints=app.HBonds)
+
+integrator = mm.LangevinIntegrator(temperature, 1.0 / u.picoseconds, 2.0 * u.femtoseconds)
+system.addForce(mm.MonteCarloBarostat(pressure, temperature, 25))
+
+simulation = app.Simulation(topology, system, integrator, platform=platform)
+simulation.context.setPositions(positions)
+simulation.context.setVelocitiesToTemperature(temperature)
+
+print("Using platform %s" % simulation.context.getPlatform().getName())
+
+simulation.reporters.append(mdtraj.reporters.DCDReporter(dcd_filename, output_frequency, atomSubset=atom_indices))
+simulation.reporters.append(app.StateDataReporter(open(log_filename, 'w'), 5000, step=True, time=True, speed=True))
+simulation.step(n_steps)
