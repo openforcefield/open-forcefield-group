cae49c2b350462dabc2cb1b18d9ac79b273510ae
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2014-05-29 19:03:03 -0400

    Some rearrangements of scalar coupling data

---
+++ nmr/ace_x_y_nh2/code/analyze_scalar_couplings.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import mdtraj as md
+from ace_x_y_nh2_parameters import *
+import scalar_couplings
+
+reference = pd.read_csv("./experimental_data/baldwin_table1_2006_couplings.csv", index_col=0)
+reference = reference["coupling"]
+
+data = []
+for (ff, water, seq) in products:
+    try:
+        aa = seq.split("_")[1]
+        t = md.load("./dcd/%s_%s_%s.dcd" % (ff, water, seq), top="./pdbs/%s.pdb" % (seq))
+    except:
+        continue
+    phi = md.compute_phi(t)[1][:, 0] * 180 / np.pi
+    psi = md.compute_psi(t)[1][:, 0] * 180 / np.pi
+    J = scalar_couplings.J3_HN_HA(phi).mean()
+    data.append([ff, water, aa, J])
+
+data = pd.DataFrame(data, columns=["ff", "water", "AA", "J"])
+
+X = data.pivot_table(values="J", cols=["AA"], rows=["ff", "water"])
+delta = X - reference
+Z = (delta / 0.36)
+rms_by_model = (Z ** 2.).mean(1) ** 0.5
+rms_by_model
