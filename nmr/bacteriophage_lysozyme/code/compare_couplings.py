191ff8b8b2661d24e29e7cea146402a66cc7c505
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2014-06-03 16:35:02 -0400

    Working J3HNHA for lambda

--- nmr/bacteriophage_lysozyme/code/compare_couplings.py
+++ nmr/bacteriophage_lysozyme/code/compare_couplings.py
@@ -9,18 +9,21 @@ print(parsed.status)
 
 q = parsed.value.saves["coupling_constant_list_1"].loops[1]
 x = pd.DataFrame(q.rows, columns=q.keys)
-x = x[["Coupling_constant.Seq_ID_1", "Coupling_constant.Seq_ID_2", "Coupling_constant.Val", "Coupling_constant.Val_err"]]
-x.rename(columns={"Coupling_constant.Seq_ID_1":"resSeq1", 
-"Coupling_constant.Seq_ID_2":"resSeq2", "Coupling_constant.Val":"value", "Coupling_constant.Val_err":"err"},
- inplace=True)
+x = x[["Coupling_constant.Seq_ID_1", "Coupling_constant.Val", "Coupling_constant.Val_err"]]
+x.rename(columns={"Coupling_constant.Seq_ID_1":"resSeq", "Coupling_constant.Val":"value", "Coupling_constant.Val_err":"err"}, inplace=True)
 
 # Need to make dtypes match to do eventual comparison.
 x["resSeq"] = x["resSeq"].astype('int')
 x["value"] = x["value"].astype('float')
 
-expt = x.set_index(["resSeq", "name"]).value
+expt = x.set_index(["resSeq"]).value
+
+top, bonds = t.top.to_dataframe()
+ind, values = md.compute_J3_HN_HA(t)
+prediction = pd.Series(values.mean(0), top.ix[ind[:,-1]].resSeq)
+
 
 delta = (expt - prediction).dropna()
 delta.name = "value"
 
-rms = (delta ** 2.).reset_index().groupby("name").value.mean() ** 0.5
+rms = (delta ** 2.).mean(0) ** 0.5
